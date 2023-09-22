1a3700b3baa6cbd75f95a053f3622dd6ff512a69
ADMIN <ADMIN@DESKTOP-0R1O7Q0>
2023-09-22 12:47:08 +0300

    Добавьте файлы проекта.

---
+++ PZ_05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(); // Дано целое число N (> 0). Найти наименьшее целое положительное число K, квадрат которого превосходит N
+            int N = 100;
+            int K = 0;
+            while (K * K <= N)
+            {// пока квадрат К не привышает заданное числo N}
+                K++; // увеличиваем К
+            }
+                K--; // уменьшаем К
+                Console.WriteLine(K); // выводим результат
+            }
+        
+    }
+}
+
+ 
+            
+
+        
+
+        
+    
